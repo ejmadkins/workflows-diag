fbe446e193de40869633e072649b7f52cc8161f5
Ed Adkins <ejmadkins@gmail.com>
2022-11-25 11:20:33 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-docker build -t ping .
-docker tag ping europe-west1-docker.pkg.dev/ejmadkins-workflows-diag/diagnostics-images/ping
-docker push europe-west1-docker.pkg.dev/ejmadkins-workflows-diag/diagnostics-images/ping
-
-docker build -t diagnostics .
-docker tag diagnostics europe-west1-docker.pkg.dev/ejmadkins-workflows-diag/diagnostics-images/diagnostics
-docker push europe-west1-docker.pkg.dev/ejmadkins-workflows-diag/diagnostics-images/diagnostics
-
